f397677be663805ba8bd8d7583aeb267a036a463
domfeng <domfeng@163.com>
2021-01-28 20:49:12 +0800

    jsTest

--- src/test/java/jsTest.java
+++ src/test/java/jsTest.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.JavascriptExecutor;
 
+
 public class jsTest extends baseTest{
     @Test
     public void jsTest(){
-        driver.get("https://www.12306.cn/index/");
-        javascriptExecutor jsDriver = (JavascriptExecutor)driver;
-        jsDriver.executeScript("")
+        try {
+            driver.get("https://www.12306.cn/index/");
+            JavascriptExecutor jsDriver = (JavascriptExecutor)driver;
+            Thread.sleep(3000);
+            jsDriver.executeScript("document.getElementById('train_date').value='2020-12-30'");
+            Thread.sleep(3000);
+            System.out.println(jsDriver.executeScript("return document.getElementById('train_date').value"));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
